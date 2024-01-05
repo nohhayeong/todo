eefb05c1b4e6e0d2f21dd3c75133dc377adbe12a
nohha0 <151031005+nohhayeong@users.noreply.github.com>
2024-01-05 11:33:46 +0900

    add id field

--- src/main/kotlin/com/teamsparta/todolist/domain/card/dto/CardResponse.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/card/dto/CardResponse.kt
@@ -1,6 +1,7 @@
 package com.teamsparta.todolist.domain.card.dto
 
 data class CardResponse(
+    val id: Long,
     val title: String,
     val content:String,
     val name:String,
--- src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/CommentResponse.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/CommentResponse.kt
@@ -1,6 +1,7 @@
 package com.teamsparta.todolist.domain.comment.dto
 
 data class CommentResponse(
+    val id: Long,
     val name:String,
     val content:String,
     val password:String
